566e404db50b07a7a7327f3c98322a0b605d6c05
HELIO LUCAS <114125117+Heliolucas@users.noreply.github.com>
2023-02-12 13:14:09 -0300

    att

---
+++ app.js
@@ -0,0 +1,9 @@
+function sol(){
+    var img = document.getElementById('img')
+    img.src = 'images/plant.jpeg'
+}
+
+function noite(){
+    var img = document.getElementById('img')
+    img.src = 'images/fotonoite.jpeg'
+}
